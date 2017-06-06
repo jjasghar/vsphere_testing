4dfe473a2e9641228e4511a91a01b71c121b0747
JJ Asghar <jj@chef.io>
2017-06-06 12:45:37 -0500

    added linux and windows specific recipes

---
+++ recipes/linux_provision.rb
@@ -0,0 +1,26 @@
+require 'chef/provisioning'
+require 'chef/provisioning/vsphere_driver'
+
+with_vsphere_driver host: '172.16.20.2',
+  insecure: true,
+  user:     'administrator@vsphere.local',
+  password: 'PASSWORD'
+
+with_machine_options :bootstrap_options => {
+                               num_cpus: 2,
+                               memory_mb: 4096,
+                               datacenter: 'Datacenter',
+                               resource_pool: 'Cluster',
+                               template_folder: 'Linux',
+                               template_name: 'ubuntu16-template',
+                               :ssh => {
+                                 :user => 'admini',
+                                 :password => 'admini',
+                                 :paranoid => false,
+                               }
+                             },
+                :sudo => true
+
+machine "testing-ubuntu" do
+  tag "haha"
+end
--- recipes/windows_provision.rb
+++ recipes/windows_provision.rb
@@ -6,25 +6,6 @@ with_vsphere_driver host: '172.16.20.2',
   user:     'administrator@vsphere.local',
   password: 'PASSWORD'
 
-with_machine_options :bootstrap_options => {
-                               num_cpus: 2,
-                               memory_mb: 4096,
-                               datacenter: 'Datacenter',
-                               resource_pool: 'Cluster',
-                               template_folder: 'Linux',
-                               template_name: 'ubuntu16-template',
-                               :ssh => {
-                                 :user => 'admini',
-                                 :password => 'admini',
-                                 :paranoid => false,
-                               }
-                             },
-                :sudo => true
-
-machine "testing-ubuntu" do
-  action :destroy
-end
-
 win_bootstrap_options = { :bootstrap_options => {
                             num_cpus: 2,
                             memory_mb: 8096,
@@ -43,12 +24,11 @@ win_bootstrap_options = { :bootstrap_options => {
                           ready_timeout: '30'
                         }
 
- with_chef_server "https://api.chef.io/organizations/ORG",
-                       :client_name => Chef::Config[:node_name],
-                       :signing_key_filename => Chef::Config[:client_key]
+with_chef_server "https://api.chef.io/organizations/ORG",
+                 :client_name => Chef::Config[:node_name],
+                 :signing_key_filename => Chef::Config[:client_key]
 
- machine "testing-windows" do
-   machine_options win_bootstrap_options
-   action :destroy
-   tag "haha"
+machine "testing-windows" do
+  machine_options win_bootstrap_options
+  tag "haha"
 end
