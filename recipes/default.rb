88b0e853da8d27ee9924f30b09034c58f397c33a
JJ Asghar <jj@chef.io>
2017-06-09 17:52:02 -0500

    rollback to 7.3.3
    Signed-off-by: JJ Asghar <jj@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 if platform?('windows')
-    windows_package "npp" do
-    source "https://notepad-plus-plus.org/repository/7.x/7.4.1/npp.7.4.1.Installer.exe"
+  windows_package "npp" do
+    #source "https://notepad-plus-plus.org/repository/7.x/7.4.1/npp.7.4.1.Installer.exe"
+    source "https://notepad-plus-plus.org/repository/7.x/7.3.3/npp.7.3.3.Installer.exe"
     action :install
   end
 else
