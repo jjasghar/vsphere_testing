654bbd79dd64b74a7d5e2e43eee681d9cdaecc99
JJ Asghar <jj@chef.io>
2017-06-12 19:20:29 -0500

    Create destory.rb

--- recipes/destory.rb
+++ recipes/destory.rb
@@ -25,30 +25,30 @@ machine "testing-ubuntu" do
   action :destroy
 end
 
-win_bootstrap_options = { :bootstrap_options => {
-                            num_cpus: 2,
-                            memory_mb: 8096,
-                            datacenter: 'Datacenter',
-                            resource_pool: 'Cluster',
-                            template_folder: 'Windows',
-                            template_name: 'windows2012R2',
-                            :ssh => {
-                              :user => 'Administrator',
-                              :password => 'Admini@',
-                              :paranoid => false,
-                              :port => "5985"
-                            }
-                          },
-                          ssh_options: { :keepalive => true, :keepalive_interval => 50, :user_known_hosts_file => '/dev/null' },
-                          ready_timeout: '30'
-                        }
-
- with_chef_server "https://api.chef.io/organizations/ORG",
-                       :client_name => Chef::Config[:node_name],
-                       :signing_key_filename => Chef::Config[:client_key]
-
- machine "testing-windows" do
-   machine_options win_bootstrap_options
-   action :destroy
-   tag "haha"
-end
+# win_bootstrap_options = { :bootstrap_options => {
+#                             num_cpus: 2,
+#                             memory_mb: 8096,
+#                             datacenter: 'Datacenter',
+#                             resource_pool: 'Cluster',
+#                             template_folder: 'Windows',
+#                             template_name: 'windows2012R2',
+#                             :ssh => {
+#                               :user => 'Administrator',
+#                               :password => 'Admini@',
+#                               :paranoid => false,
+#                               :port => "5985"
+#                             }
+#                           },
+#                           ssh_options: { :keepalive => true, :keepalive_interval => 50, :user_known_hosts_file => '/dev/null' },
+#                           ready_timeout: '30'
+#                         }
+# 
+#  with_chef_server "https://api.chef.io/organizations/ORG",
+#                        :client_name => Chef::Config[:node_name],
+#                        :signing_key_filename => Chef::Config[:client_key]
+# 
+#  machine "testing-windows" do
+#    machine_options win_bootstrap_options
+#    action :destroy
+#    tag "haha"
+# end
