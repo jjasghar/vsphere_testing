fb92580cf5e248a368f35afcde2fe70f5369eff4
JJ Asghar <jj@chef.io>
2017-06-26 18:15:17 -0500

    Update linux_provision.rb

--- recipes/linux_provision.rb
+++ recipes/linux_provision.rb
@@ -6,21 +6,21 @@ with_vsphere_driver host: '10.0.0.102',
   user:     'administrator@vsphere.local',
   password: 'PASSWORD'
 
-with_machine_options :bootstrap_options => {
-                               num_cpus: 2,
-                               memory_mb: 4096,
-                               datacenter: 'Datacenter',
-                               resource_pool: 'Cluster',
-                               template_folder: 'Linux',
-                               template_name: 'ubuntu16-template',
-                               :ssh => {
-                                 :user => 'admini',
-                                 :password => 'admini',
-                                 :paranoid => false,
-                               }
-                             },
-                :sudo => true
+#with_machine_options :bootstrap_options => {
+#                               num_cpus: 2,
+#                               memory_mb: 4096,
+#                               datacenter: 'Datacenter',
+#                               resource_pool: 'Cluster',
+#                               template_folder: 'Linux',
+#                               template_name: 'ubuntu16-template',
+#                               :ssh => {
+#                                 :user => 'admini',
+#                                 :password => 'admini',
+#                                 :paranoid => false,
+#                               }
+#                             },
+#                :sudo => true
 
-machine "testing-ubuntu" do
-  tag "haha"
-end
+#machine "testing-ubuntu" do
+#  tag "haha"
+#end
